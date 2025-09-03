8464a7e0e9ab8d9ec26a67db454f95e1898a646b
ZezoNone <145016983+ZezoNone@users.noreply.github.com>
2025-09-03 15:11:29 +0800

    Add files via upload

---
+++ lab4/assignment/assignment2/src/utils/asm_utils.asm
@@ -0,0 +1,78 @@
+[bits 32]
+
+global asm_hello_world
+global asm_id
+
+asm_hello_world:
+    push eax
+    xor eax, eax
+
+    mov ah, 0x03 ;青色
+    mov al, 'H'
+    mov [gs:2 * 0], ax
+
+    mov al, 'e'
+    mov [gs:2 * 1], ax
+
+    mov al, 'l'
+    mov [gs:2 * 2], ax
+
+    mov al, 'l'
+    mov [gs:2 * 3], ax
+
+    mov al, 'o'
+    mov [gs:2 * 4], ax
+
+    mov al, ' '
+    mov [gs:2 * 5], ax
+
+    mov al, 'W'
+    mov [gs:2 * 6], ax
+
+    mov al, 'o'
+    mov [gs:2 * 7], ax
+
+    mov al, 'r'
+    mov [gs:2 * 8], ax
+
+    mov al, 'l'
+    mov [gs:2 * 9], ax
+
+    mov al, 'd'
+    mov [gs:2 * 10], ax
+
+    pop eax
+    ret
+
+    asm_id:
+    push eax
+    xor eax, eax
+
+    mov ah, 0x74
+    mov al, '2'
+    mov [gs:2 * 0], ax
+
+    mov al, '3'
+    mov [gs:2 * 1], ax
+
+    mov al, '3'
+    mov [gs:2 * 2], ax
+
+    mov al, '3'
+    mov [gs:2 * 3], ax
+
+    mov al, '6'
+    mov [gs:2 * 4], ax
+
+    mov al, '3'
+    mov [gs:2 * 5], ax
+
+    mov al, '4'
+    mov [gs:2 * 6], ax
+
+    mov al, '2'
+    mov [gs:2 * 7], ax
+
+    pop eax
+    ret
+
